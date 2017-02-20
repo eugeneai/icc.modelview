a4fe311522e8817925e0b7b2bb3d8f07a69036f2
Evgeny Cherkashin <eugene@irnok.net>
2017-02-20 09:20:33 +0800

    Basic interface definitions

--- src/icc/modelview/interfaces.py
+++ src/icc/modelview/interfaces.py
@@ -1,7 +1,24 @@
 from zope.interface import Interface, Attribute
+
 class IModel(Interface):
-    """Just marker interface, to mark models
+    """Just marker interface, to mark models in
+    various MV* patterns: MVC, MVP, MVMC, etc.
     """
 
-class IModuleModel(IModel):
-    """Module model """
+
+class IView(Interface):
+    """Interface representing view in MVC, MVP, etc.
+    """
+    model = Attribute("Project under exploration. The MVC Model.")
+    ui = Attribute("User interface component holder.")
+
+
+class IController(Interface):
+    """Interface representing Controller in MVC
+    """
+
+
+class IPresenter(IController):
+    """Interface representing Presenter in MVP
+    """
+
